828851cbc6cdda4afa4b4cb42361aee65ba5ec27
HuyenTrang-3331 <htrang11022004@gmail.com>
2026-03-20 16:03:59 +0700

    feat: login

---
+++ backend/src/main/java/com/example/demo/HuyenTrangApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuyenTrangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuyenTrangApplication.class, args);
+	}
+
+}
